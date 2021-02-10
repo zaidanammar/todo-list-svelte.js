4b45d0c8d7935571bd053693e0d4beb6c02cb22a
zaidanammar <zaidanammar23@gmail.com>
2021-02-10 09:45:34 +0700

    update config

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 
 const http = require('http')
 const app = require('./app')
-const PORT = process.env.PORT || 3000
+const port = process.env.PORT || 3000
 const server = http.createServer(app)
 
-server.listen(PORT, () => console.log(`this app running on PORT ${PORT}`))
+server.listen(port, () => console.log(`this app running on PORT ${port}`))
